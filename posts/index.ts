f9e3684691e5b6c7a6b98ace96ac27642c656309
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-11-02 19:40:25 -0400

    📦 NEW: posts placeholder code added

---
+++ posts/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/posts", (req, res) => {});
+
+app.post("/posts", (req, res) => {});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
