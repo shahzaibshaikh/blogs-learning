fa00e2b7867b7e865bb63bc4d56fb1166ab645a2
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-11-02 20:38:39 -0400

    👌 IMPROVE: implemented posts service

--- posts/index.ts
+++ posts/index.ts
@@ -1,9 +1,33 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
 const app = express();
 
-app.get("/posts", (req, res) => {});
+interface Post {
+  [key: string]: {
+    id: string;
+    title: string;
+  };
+}
 
-app.post("/posts", (req, res) => {});
+const posts: Post = {};
+app.use(express.json());
+
+app.get("/posts", (req: Request, res: Response) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req: Request, res: Response) => {
+  const id = uuidv4();
+  const title = req.body.title;
+
+  posts[id] = {
+    id,
+    title
+  };
+  console.log(posts);
+
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
   console.log("Listening on 4000");
