b75bb94ff06c00ae0cbae77a042bf867cfc7b245
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-11-04 14:17:41 -0400

    👌 IMPROVE: added dependencies

--- query/index.ts
+++ query/index.ts
@@ -8,7 +8,9 @@ app.use(cors());
 
 app.get("/posts", (req: Request, res: Response) => {});
 
-app.post("/posts", (req: Request, res: Response) => {});
+app.post("/posts", (req: Request, res: Response) => {
+  const { type, data } = req.body;
+});
 
 app.listen(4002, () => {
   console.log("Listening on 4002");
