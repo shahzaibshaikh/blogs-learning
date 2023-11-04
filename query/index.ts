eaa4338284c2e778bd6bb93f829f57a378aeb4f3
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-11-04 11:40:29 -0400

    📦 NEW: added placeholder routes in query service

---
+++ query/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/posts", (req: Request, res: Response) => {});
+
+app.post("/posts", (req: Request, res: Response) => {});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
