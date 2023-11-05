3f9d5b4c99da8486ed9419bb0d3fd755318097a2
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-11-05 00:12:19 -0400

    👌 IMPROVE:  handled PostCreated event

--- query/index.ts
+++ query/index.ts
@@ -6,10 +6,32 @@ app.use(express.json());
 
 app.use(cors());
 
+interface Posts {
+  [postId: string]: {
+    id: string;
+    title: string;
+    comments: Comment[];
+  };
+}
+
+interface Comment {
+  id: string;
+  content: string;
+}
+
+const posts: Posts = {};
+
 app.get("/posts", (req: Request, res: Response) => {});
 
 app.post("/posts", (req: Request, res: Response) => {
   const { type, data } = req.body;
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+  }
 });
 
 app.listen(4002, () => {
