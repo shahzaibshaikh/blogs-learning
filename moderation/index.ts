5f6cee253719746211a6c74c134cdcae048a0f0b
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-11-06 21:44:16 -0500

    👌 IMPROVE: implemented moderation service logic

--- moderation/index.ts
+++ moderation/index.ts
@@ -4,7 +4,21 @@ import axios from "axios";
 const app = express();
 app.use(express.json());
 
-app.post("/events", (req: Request, res: Response) => {});
+app.post("/events", async (req: Request, res: Response) => {
+  const { type, data } = req.body;
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
+  }
+});
 
 app.listen(4003, () => {
   console.log("Listening on 4003");
