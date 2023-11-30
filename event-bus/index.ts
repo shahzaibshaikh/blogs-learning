abf5fdc5261c9ea817a2f47eba0a5f8b18bb8e87
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-11-30 18:04:59 -0500

    👌 IMPROVE: modified URL of posts cluster ip

--- event-bus/index.ts
+++ event-bus/index.ts
@@ -11,10 +11,10 @@ const events: any = [];
 app.post("/events", (req: Request, res: Response) => {
   const event = req.body;
   events.push(event);
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events", event);
+  axios.post("http://posts-clusterip-srv:4000/events", event);
+  // axios.post("http://localhost:4001/events", event);
+  // axios.post("http://localhost:4002/events", event);
+  // axios.post("http://localhost:4003/events", event);
 
   res.send({ status: "OK" });
 });
