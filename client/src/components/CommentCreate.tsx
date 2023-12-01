c648c22100d74d72a384f8fdea6a6025b4dce35b
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-12-01 16:09:42 -0500

    👌 IMPROVE: modified URL in commentCreate

--- client/src/components/CommentCreate.tsx
+++ client/src/components/CommentCreate.tsx
@@ -10,7 +10,7 @@ function CommentCreate({ postId }: CommentCreateProps) {
 
   const onSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+    await axios.post(`http://posts.com/posts/${postId}/comments`, {
       content
     });
     setContent("");
