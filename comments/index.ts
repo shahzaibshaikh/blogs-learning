8631b977018c8b60cc2f00062ae85ca5782a6424
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-11-30 22:08:26 -0500

    👌 IMPROVE: updated URL for event-bus

--- comments/index.ts
+++ comments/index.ts
@@ -30,7 +30,7 @@ app.post("/posts/:id/comments", async (req: Request, res: Response) => {
   comments.push({ id: commentId, content, status: "pending" });
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "CommentCreated",
     data: {
       id: commentId,
@@ -59,7 +59,7 @@ app.post("/events", async (req: Request, res: Response) => {
       comment.status = status;
     }
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       type: "CommentUpdated",
       data: {
         id,
